9d31989530d50f55d0049ac927da5c931a891483
ValonJashari <48812529+ValonJashari@users.noreply.github.com>
2021-03-08 15:13:34 +0100

    Add files via upload
    Intermediates:
    Excercise2
    Exercise3

--- Vali/int_exc2.js
+++ Vali/int_exc2.js
@@ -1,12 +1,28 @@
-for(let i=0; i<101;i++){
+for(let i=1; i<101;i++){
     if(i%3==0){
         console.log("Fizz");
+        document.getElementById("Fizz").innerHTML+="Fizz<br>";
     }
     else if(i%5==0){
         console.log("Buzz");
+        document.getElementById("Fizz").innerHTML+="Buzz<br>";
     }
     else {
         console.log(i);
+        document.getElementById("Fizz").innerHTML+=i+"<br>";
     }
 }
+
+
+// Exerise 3
+var b = ""
+for(let i=0; i<6;i++)
+{    b +='*';
+    document.getElementById('Star').innerHTML+=b+"<br>"
+    console.log("*")
+   
+    
+}
+
+if()
 
